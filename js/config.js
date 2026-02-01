60986a7d48502712a6e353820295047d0dbdaba3
Jbrimz088 <167040831+Jbrimz088@users.noreply.github.com>
2026-02-01 12:28:33 -0500

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,3 @@
+const API_KEY = '670f376983f73b67d757e9a951a3a5a9'; // Replace with your actual TMDb API key
+    const BASE_URL = 'https://api.themoviedb.org/3';
+    const IMAGE_BASE = 'https://image.tmdb.org/t/p/w500';
